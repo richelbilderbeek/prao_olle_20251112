691f38f8e7b43bfb96c4b0ac8a1ad89f84658282
Olle Modin <olle.modin-lindgren@skola.uppsala.se>
2025-11-12 14:03:33 +0100

    Markdown sucks

--- project/warner/src/mixed.rs
+++ project/warner/src/mixed.rs
@@ -1,11 +1,5 @@
-use std::fs;
+use std::fs::{self, rename};
 
 pub fn warn(input_file_contents: String) -> String{
-    let split: Vec<&str> = input_file_contents.split("\n").collect();
-    let mut final_string = String::new();
-    for line in split {
-        final_string += &(line.to_owned() + "<!-- DO NOT EDIT -->" + "\n");
-    }
-    println!("{}", final_string);
-final_string
+    "<!-- DO NOT EDIT -->".to_owned() + &input_file_contents
 }
