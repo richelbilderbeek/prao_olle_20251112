234350cc7e025b5a87cbd7caaa362c65ba19eaca
Olle Modin <olle.modin-lindgren@skola.uppsala.se>
2025-11-12 11:22:52 +0100

    Testing

--- project/warner/src/main.rs
+++ project/warner/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, fs::{self, OpenOptions}, io::{Read, Write}};
+use std::{env::args, fs::{self}, io::Read};
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args: Vec<String> = args().collect();
@@ -11,19 +11,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         let mut split: Vec<String> = file_contents.split("\n").map(|v| v.to_string()).collect();
         split.retain(|v| v != "");
         #[allow(unused_assignments)]
-        let mut i = 0;
-        while i < split.len() {
-            split[i] = "# DO NOT EDIT".to_owned() + &split[i];
-            i += 1;
-        }
-        let mut options = OpenOptions::new();
-        options.append(true);
-        options.create_new(true);
-        let mut f = options.open("./output.md")?;
-        for v in split {
-            f.write((v + "\n").as_bytes())?;
-        }
+        let mut final_string = String::new();
+        for part in split {
+            let mut restitched: String = String::new();
+            let mut pipes_split: Vec<String> = part.split("|").map(|v| v.to_owned()).collect();
+            pipes_split.retain(|v| v != "");
+            pipes_split[2] = "(DO NOT EDIT)".to_owned() + &pipes_split[2];
+            let mut i = 0;
+            let len = pipes_split.len();
+            for mut x in pipes_split {
+                if  i < len{
+                    x += "|";
 
+                }
+                restitched += &x;
+                i += 1;
+            }
+            final_string += &(restitched + "\n");
+            fs::write("output.md", &final_string)?;
+        }
         return Ok(());
     }
     panic!("No file was supplied")
