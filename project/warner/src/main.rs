68c575b47787724b614db9e1771ec6e81c0f1af9
Olle Modin <olle.modin-lindgren@skola.uppsala.se>
2025-11-12 14:04:35 +0100

    DO NOT EDIT

--- project/warner/src/main.rs
+++ project/warner/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, fs::{self}, io::Read, process::exit};
+use std::{env::args, fs::{self, rename}, io::Read, process::exit};
 
 use crate::mixed::warn;
 mod mixed;
@@ -7,6 +7,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args: Vec<String> = args().collect();
     if args.get(1) != None {
         fs::write(&args[1], warn(fs::read_to_string(&args[1])?))?;
+        rename(&args[1], "COURSES_DO_NOT_EDIT.md")?;
         return Ok(());
     }
     panic!()
