fd0afc188e749ca08e9c7e992ee1690651faee98
Olle Modin <olle.modin-lindgren@skola.uppsala.se>
2025-11-12 13:53:28 +0100

    a

--- project/warner/src/main.rs
+++ project/warner/src/main.rs
@@ -1,41 +1,13 @@
-use std::{env::args, fs::{self}, io::Read};
+use std::{env::args, fs::{self}, io::Read, process::exit};
+
+use crate::mixed::warn;
 mod mixed;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args: Vec<String> = args().collect();
-    let file = args.get(1);
-    let output_file = args.get(2);
-    if file != None  && output_file != None{
-        let file_path = file.expect("No input file was supplied");
-        let mut file = fs::File::open(file_path)?;
-        let mut file_contents = String::new();
-        file.read_to_string(&mut file_contents)?;
-        let mut split: Vec<String> = file_contents.split("\n").map(|v| v.to_string()).collect();
-        split.retain(|v| v != "");
-        split.remove(0);
-        split.remove(0);
-        #[allow(unused_assignments)]
-        let mut final_string = String::new();
-        final_string += "|**From**|**To**|**Course name**|**Course site**|**Provider site**|**Provider name**|\n|:----------|:----------|:---------------------------------------------------------------------------|:---------------|:-----------------|:-----------------|\n";
-        for part in split {
-            let mut restitched: String = String::new();
-            let mut pipes_split: Vec<String> = part.split("|").map(|v| v.to_owned()).collect();
-            pipes_split.retain(|v| v != "");
-            pipes_split[2] = "<!-- DO NOT EDIT -->".to_owned() + &pipes_split[2];
-            let mut i = 0;
-            let len = pipes_split.len();
-            for mut x in pipes_split {
-                if  i < len{
-                    x += "|";
-                }
-                restitched += &x;
-                i += 1;
-            }
-            final_string += &(restitched + "\n");
-        }
-        fs::write(output_file.expect("No output file specified"), &final_string)?;
-
+    if args.get(1) != None {
+        fs::write(&args[1], warn(fs::read_to_string(&args[1])?))?;
         return Ok(());
     }
-    panic!("Either output or input file was not specified")
+    panic!()
 }
--- project/warner/src/mixed.rs
+++ project/warner/src/mixed.rs
@@ -1,10 +1,11 @@
 use std::fs;
 
-pub fn warn(input_file_contents: String, output_file_path: String) {
-    let split: Vec<String> = input_file_contents.split("\n").map(|v| v.to_owned()).collect();
+pub fn warn(input_file_contents: String) -> String{
+    let split: Vec<&str> = input_file_contents.split("\n").collect();
     let mut final_string = String::new();
-    for line in  split{
-        final_string += &(line + "\n<!-- DO NOT EDIT -->\n");
+    for line in split {
+        final_string += &(line.to_owned() + "<!-- DO NOT EDIT -->" + "\n");
     }
-    fs::write(output_file_path, final_string).unwrap();
+    println!("{}", final_string);
+final_string
 }
